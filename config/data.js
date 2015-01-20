aa5209e33ef97a6e061d249e74b0e65d4e831b00
njdup <njdupp@gmail.com>
2015-01-20 00:05:56 -0800

    Making changes to skills and projects

--- config/data.js
+++ config/data.js
@@ -18,12 +18,12 @@ module.exports = {
       'links': [
         {'name':'github', 'url':'https://github.com/njdup/PersonalKnowledgeBase'}
       ],
-      'skills': ['python', 'javascript', 'node.js', 'mongodb', 'express.js']
+      'skills': ['python', 'javascript', 'node.js', 'mongodb', 'express.js', 'machine learning']
     },
     {
       'title': 'Wellness',
       'tagline': 'An application to better people\'s lives',
-      'description': 'Work in progress',
+      'description': 'Towards the end of this past summer, I had a thought: "It would be really nice if I had a way of tracking my mood from day to day and the activities I did each day, with the end goal of identifying trends in my mood and how certain activities affect it..." That\'s where Wellness came in. While its a personal project currently on hiatus, the end goal is to create a system with which people can track their moods and learn what upsets them or uplifts them. I also plan to apply some machine learning techniques I\'ve picked up in recent courses to make suggestions on what activities seem to improve a user\'s mood when that user seems down.',
       'links': [
         {'name': 'github', 'url': 'https://github.com/njdup/wellness'}
       ],
@@ -43,15 +43,20 @@ module.exports = {
       'type': 'framework',
       'description': 'Most of my full stack web dev experience has been spent in django-land. I\'ve grown to love its power and ease, but lately have been put off by some of its bloated qualities as I\'ve shifted further towards an appreciation of lightweight applications.'
     },
-    { 'name': 'node.js',
+    { 'name': 'node.js/express.js',
       'type': 'framework',
-      'description': 'When it\'s time to get down to business and serve up a reliable, simple web application, I turn to node.js. My experience with it is still pretty elementary, but I\'ve enjoyed it a lot in the work that I have done with it, including the development of this site.'
+      'description': 'When it\'s time to get down to business and serve up a reliable, simple web application, I turn to the node.js + express.js. My experience with it is still pretty elementary, but I\'ve enjoyed it a lot in the work that I have done with it, including the development of this site.'
     },
     {
-      'name': 'Test',
-      'type': 'framework',
-      'description': 'This is a test!'
+      'name': 'golang',
+      'type': 'programming language',
+      'description': 'Such a fun new language with a cute as hell mascot to boot. I\'ve been wanting to pick up Go for a while now - ever since I got a chance to learn about it from some Paypal engineers who seemed absolutely infatuated with it - and finally got around to it recently. I\'ve immediately grown to love the completeness of its standard libraries, and I find its syntax and conventions quite nifty. I haven\'t dabbled with goroutines and multithreading it, but even without those specific language advantages, I can already see the allure of using go for new web applications.'
     },
+    {
+      'name': 'mongoDB',
+      'type': 'technology',
+      'description': 'Mongo is my go-to datastore for personal projects. The usual allures have drawn me to it (its ease of use, the ability to throw JSON straight from the client into the database, and its schemaless design to name a few), but the more I\'ve used it, the more I\'ve realized that its approach to storing data and information just seems to be a better match with my mental model of storing information than the alternatives.'
+    }
   ],
   interests: [
     {
